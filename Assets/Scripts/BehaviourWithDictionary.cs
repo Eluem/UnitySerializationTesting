e16c6ec09c035766a33c2a617b2c781201a774a2
Eluem <eulmmm@hotmail.com>
2025-04-11 17:43:34 -0400

    Initial commit

---
+++ Assets/Scripts/BehaviourWithDictionary.cs
@@ -0,0 +1,25 @@
+using AYellowpaper.SerializedCollections;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BehaviourWithDictionary : MonoBehaviour
+{
+    //Hitting the plus and creating a new entry seems to create a new key/value pair but the defaults aren't based on the default values set at the top of the class
+    [SerializedDictionary("Name", "Data")]
+    [SerializeField] SerializedDictionary<string, CustomClass> _testSerializedDictionary = new SerializedDictionary<string, CustomClass>();
+
+    [SerializeField] CustomClass _CustomClassGoodDefaults = new CustomClass(); //If I initialize it, unity correctly uses the defaults
+    [SerializeField] CustomClass _CustomClassBadDefaults; //If I leave it null and let unity assume the defaults, they end up standard (0 for the floats in this case)
+
+
+    public CustomClass GetData(string pName)
+    {
+        if(_testSerializedDictionary.ContainsKey(pName))
+        {
+            return _testSerializedDictionary[pName];
+        }
+
+        return null;
+    }
+}
---
+++ Assets/Scripts/CustomClass.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+[System.Serializable]
+public class CustomClass
+{
+    [SerializeField] float _someFloatOne = 1; //These default values are all it should take to get unity to respect the defaults correctly (if the variable is initialized)
+    [SerializeField] float _someFloatTwo = 2;
+
+    public float SomeFloatOne
+    {
+        get
+        {
+            return _someFloatOne;
+        }
+    }
+
+    public float SomeFloatTwo
+    {
+        get
+        {
+            return _someFloatTwo;
+        }
+    }
+
+
+    /*
+    public CustomClass(float pSomeFloatOne = 1, float pSomeFloatTwo = 2)
+    {
+        _someFloatOne = pSomeFloatOne;
+        _someFloatTwo = pSomeFloatTwo;
+    }
+    */
+}
